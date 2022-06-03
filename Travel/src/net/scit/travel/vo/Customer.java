e8012a8e63d7bf3f67dd90af45f6b4050b64f817
user <user@10.10.5.175>
2022-06-03 13:20:47 +0900

    06/03 13:20

---
+++ Travel/src/net/scit/travel/vo/Customer.java
@@ -0,0 +1,9 @@
+package net.scit.travel.vo;
+
+public class Customer {
+	private String customer_id;
+	private String customer_pw;
+	private String customer_name;
+	private String customer_email;
+	
+}
---
+++ Travel/src/net/scit/travel/vo/Goods_List.java
@@ -0,0 +1,9 @@
+package net.scit.travel.vo;
+
+public class Goods_List {
+	private int course_num;
+	private String course_title;
+	private String course_text;
+	private int course_clickcount;
+	
+}
